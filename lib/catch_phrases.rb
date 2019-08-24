5ec0aaad0d2d4fe8f2f2f5e00752763fc5676903
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-24 20:26:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,17 @@
-def mario
+def mario do
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool
+def toadstool do
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-def link
+def link do
   puts 'It's Dangerous To Go Alone! Take This.'
+end
+
+def any_phrase do 
+
 end
